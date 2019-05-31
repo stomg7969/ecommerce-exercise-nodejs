28a86750fb314dc4495a0796420f14b4e63de500
stomg7969 <stomg7969@gmail.com>
2019-05-31 00:51:16 -0400

    changes in extensions

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,82 +1,83 @@
-const Product = require("../models/product");
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  console.log("ADMIN, ADD PRODUCT...");
-  // res.send(
-  // '<form action="/admin/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
-  // );
-  // next() argument allows the request to continue to the next middleware in line
-  // We can't use next() when response is already called. Will get an error.
-  // BELOW: when I use plain html
-  // res.sendFile(path.join(rootDir, 'views', 'product.html'));
-  // BELOW: when I use pug template.
-  res.render("admin/edit-product", {
-    pageTitle: "ADD PRODUCT",
-    path: "/admin/add-product",
-    editing: false
-  });
+	console.log('ADMIN, ADD PRODUCT...');
+	// res.send(
+	// '<form action="/admin/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+	// );
+	// next() argument allows the request to continue to the next middleware in line
+	// We can't use next() when response is already called. Will get an error.
+	// BELOW: when I use plain html
+	// res.sendFile(path.join(rootDir, 'views', 'product.html'));
+	// BELOW: when I use pug template.
+	res.render('admin/edit-product', {
+		pageTitle: 'ADD PRODUCT',
+		path: '/admin/add-product',
+		editing: false
+	});
 };
 
 exports.postAddProduct = (req, res) => {
-  // renders after submitting.
-  console.log('ADMIN POST CONTROLLER ', req.body); // this is undefined unless I have parse.
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  // const { title, imageUrl, price, description } = req.body;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => console.log('HAS ERR IN ADMIN CTRL?', err));
+	// renders after submitting.
+	console.log('ADMIN POST CONTROLLER ', req.body); // this is undefined unless I have parse.
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
+	// const { title, imageUrl, price, description } = req.body;
+	const product = new Product(null, title, imageUrl, description, price);
+	product
+		.save()
+		.then(() => {
+			res.redirect('/');
+		})
+		.catch((err) => console.log('HAS ERR IN ADMIN CTRL?', err));
 };
 
 exports.getEditProduct = (req, res, next) => {
-  // .query is for checking query params in the url.
-  // so this path will work only when url has .../:id?edit=true
-  const editMode = req.query.edit;
-  console.log("DOES QUERY URL HAS '?edit=true'?", editMode)
-  if (!editMode) return res.redirect('/');
+	// .query is for checking query params in the url.
+	// so this path will work only when url has .../:id?edit=true
+	const editMode = req.query.edit;
+	console.log("DOES QUERY URL HAS '?edit=true'?", editMode);
+	if (!editMode) return res.redirect('/');
 
-  const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) return res.redirect('/');
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit PRODUCT',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-    // Remember, res.render is what I send information to view pages.
-  });
+	const prodId = req.params.productId;
+	Product.findById(prodId, (product) => {
+		if (!product) return res.redirect('/');
+		res.render('admin/edit-product', {
+			pageTitle: 'Edit PRODUCT',
+			path: '/admin/edit-product',
+			editing: editMode,
+			product: product
+		});
+		// Remember, res.render is what I send information to view pages.
+	});
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const { productId, title, price, imageUrl, description } = req.body;
-  const updatedProduct = new Product(productId, title, imageUrl, description, price);
-  updatedProduct.save();
-  // Instead of redirecting all the time, validate if .save() or .delete() is successful first.
-  res.redirect('/admin/products');
+	const { productId, title, price, imageUrl, description } = req.body;
+	const updatedProduct = new Product(productId, title, imageUrl, description, price);
+	updatedProduct.save();
+	// Instead of redirecting all the time, validate if .save() or .delete() is successful first.
+	res.redirect('/admin/products');
 };
 
 exports.postDeleteProduct = (req, res) => {
-  console.log('DELETING(in admin contlr).', req.body);
-  // I am also passing price to update cart.
-  // this is something I am trying on my own, not instructor's
-  const { productId, productPrice } = req.body;
-  Product.delete(productId, parseFloat(productPrice));
-  res.redirect('/admin/products');
-}
+	console.log('DELETING(in admin contrl).', req.body);
+	// I am also passing price to update cart.
+	// this is something I am trying on my own, not instructor's
+	const { productId, productPrice } = req.body;
+	Product.delete(productId, parseFloat(productPrice));
+	res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res, next) => {
-  console.log('Product list for admin')
-    Product.fetchAll(products => {
-        res.render('admin/products', {
-            products: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products'
-        })
-    })
+	console.log('Product list for admin');
+	Product.fetchAll((products) => {
+		res.render('admin/products', {
+			products: products,
+			pageTitle: 'Admin Products',
+			path: '/admin/products'
+		});
+	});
 };
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,10 +1,13 @@
 // const path = require("path");
 // const rootDir = require("../helper/path");
 const Product = require("../models/product");
-const Cart = require('../models/cart');
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  console.log("%c All products(shop ctrl)", "color: white; background-color: black");
+  console.log(
+    "%c All products(shop ctrl)",
+    "color: white; background-color: black"
+  );
   Product.fetchAll()
     .then(([rows, fieldData]) => {
       res.render("shop/product-list", {
@@ -13,22 +16,22 @@ exports.getProducts = (req, res, next) => {
         path: "/products"
       });
     })
-    .catch(err => console.log('HAS ERR IN getProducts?', err));
+    .catch(err => console.log("HAS ERR IN getProducts?", err));
 };
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-  //  Reason for [product] is ... it's like const { value } = req.body;
+    //  Reason for [product] is ... it's like const { value } = req.body;
     .then(([product]) => {
-      res.render('shop/product-detail', { 
+      res.render("shop/product-detail", {
         // [product] will return the first element only, but in array. So I need to get the first index.
-        product: product[0], 
+        product: product[0],
         pageTitle: product.title,
-        path: '/products'
+        path: "/products"
       });
     })
-    .catch(err => console.log('HAS ERR IN getProduct?', err));
-}
+    .catch(err => console.log("HAS ERR IN getProduct?", err));
+};
 exports.getIndex = (req, res, next) => {
   // Remember that the data is stored in first element. Instructor calls it rows.
   Product.fetchAll()
@@ -40,8 +43,8 @@ exports.getIndex = (req, res, next) => {
         path: "/"
       });
     })
-    .catch(err => console.log('HAS ERR IN getIndex?', err));
-}
+    .catch(err => console.log("HAS ERR IN getIndex?", err));
+};
 exports.getCart = (req, res, next) => {
   // Here, I am rendering list of products that is in the cart.json.
   // But I also have to fetch from products because cart.json doesn't contain detail product info.
@@ -51,25 +54,27 @@ exports.getCart = (req, res, next) => {
       // by creating empty array, there won't be an error even if cart is empty.
       const cartProducts = [];
       for (const product of products) {
-        const cartProductObj = cart.products.find(prod => prod.id === product.id);
+        const cartProductObj = cart.products.find(
+          prod => prod.id === product.id
+        );
         if (cartProductObj) {
-          cartProducts.push({ product, qty: cartProductObj.qty })
+          cartProducts.push({ product, qty: cartProductObj.qty });
         }
       }
-      res.render('shop/cart', {
-        pageTitle: 'Cart',
-        path: '/cart',
+      res.render("shop/cart", {
+        pageTitle: "Cart",
+        path: "/cart",
         products: cartProducts
       });
     });
   });
-}
+};
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, product => {
     Cart.addProduct(prodId, product.price);
   });
-  res.redirect('/cart');
+  res.redirect("/cart");
 };
 exports.postCartDeleteProduct = (req, res) => {
   // Again, like delete product, I can use hidden input for price then pass it.
@@ -78,18 +83,18 @@ exports.postCartDeleteProduct = (req, res) => {
   // findById is static custom function.
   Product.findById(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
+    res.redirect("/cart");
   });
-}
+};
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    pageTitle: 'Checkout',
-    path: '/checkout'
+  res.render("shop/checkout", {
+    pageTitle: "Checkout",
+    path: "/checkout"
   });
 };
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    pageTitle: 'Orders',
-    path: '/orders'
+  res.render("shop/orders", {
+    pageTitle: "Orders",
+    path: "/orders"
   });
 };
