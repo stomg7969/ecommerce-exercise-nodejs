6d7df1387bedbdb6acc1702227420bce9d0a6832
stomg7969 <stomg7969@gmail.com>
2019-06-03 19:54:54 -0400

    Added some comments about cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.get('Cookie').split('=')[1]);
+  // const isLoggedIn = req.get('Cookie').split(';')[1].split('=')[1];
   res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login',
@@ -6,6 +8,9 @@ exports.getLogin = (req, res, next) => {
   });
 };
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true') // LIke localStorage.setItem / removeItem.
+  // I can check the cookie in Chrome -> Application -> Cookies -> <the URL I am using>.
+  // I can grab that cookie if I want  ==> console.log(req.get('Cookie').split(';')[1].split('=')[1]);
+  // req.isLoggedIn = true; // this data is lost after the request, like state in react. // That is where cookies or session come in.
   res.redirect('/');
 };
