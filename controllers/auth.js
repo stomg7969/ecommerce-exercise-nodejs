04ac8a570eb0dfaa4f19f41bf3f3cbf9a4f8189e
stomg7969 <stomg7969@gmail.com>
2019-06-03 20:04:12 -0400

    added more comment and grab boolean

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.get('Cookie').split('=')[1]);
-  // const isLoggedIn = req.get('Cookie').split(';')[1].split('=')[1];
+  const isLoggedIn = req.get('Cookie').split('=')[1]; // Be aware that there might be MULTIPLE COOKIES.
   res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login',
@@ -10,7 +9,8 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   res.setHeader('Set-Cookie', 'loggedIn=true') // LIke localStorage.setItem / removeItem.
   // I can check the cookie in Chrome -> Application -> Cookies -> <the URL I am using>.
-  // I can grab that cookie if I want  ==> console.log(req.get('Cookie').split(';')[1].split('=')[1]);
+  // I can grab that cookie if I want  ==> console.log(req.get('Cookie').split('=')[1]);
+  // IMPORTANT: Cookies can be MANIPULATED by Users from their Browser. So store only NOT important ones like cart items.
   // req.isLoggedIn = true; // this data is lost after the request, like state in react. // That is where cookies or session come in.
   res.redirect('/');
 };
