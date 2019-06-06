d8e4ccc7fdfe0712053ed5ff63348b7e9a85361e
stomg7969 <stomg7969@gmail.com>
2019-06-06 10:49:47 -0400

    Added Authentication process on route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
+// isAuth checks if user is authorized to have an access.
+const isAuth = require('../middleware/is-auth');
 
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
@@ -16,7 +18,7 @@ router.post('/reset', authController.postReset);
 router.get('/reset/:token', authController.getNewPassword);
 router.post('/new-password', authController.postNewPassword);
 
-router.get('/update-password', authController.getUpdatePassword)
+router.get('/update-password', isAuth, authController.getUpdatePassword)
 router.post('/update-password', authController.postUpdatePassword)
 
 module.exports = router;
